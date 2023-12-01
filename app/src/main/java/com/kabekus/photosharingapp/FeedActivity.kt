6fdd2c8d0fd4528ed4aeb67ded0adaf4c0adbb5a
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2023-12-01 15:26:50 +0300

    Created UI for the application

--- app/src/main/java/com/kabekus/photosharingapp/FeedActivity.kt
+++ app/src/main/java/com/kabekus/photosharingapp/FeedActivity.kt
@@ -2,24 +2,15 @@ package com.kabekus.photosharingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import com.kabekus.photosharingapp.databinding.ActivityFeedBinding
 import com.kabekus.photosharingapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class FeedActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityFeedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
-
-    fun signInClicked(view : View){
-
-    }
-
-    fun signUpClicked(view: View){
-
-    }
-
 }
---
+++ app/src/main/java/com/kabekus/photosharingapp/UploadActivity.kt
@@ -0,0 +1,24 @@
+package com.kabekus.photosharingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.kabekus.photosharingapp.databinding.ActivityFeedBinding
+import com.kabekus.photosharingapp.databinding.ActivityUploadBinding
+
+class UploadActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityUploadBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun postUpload(view: View){
+
+    }
+    fun selectImage(view: View){
+        
+    }
+}
