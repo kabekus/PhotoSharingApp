592ad35ae0263f72da2544a044284eac32064de9
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2023-12-06 17:40:26 +0300

    Using Fire Storage

--- app/src/main/java/com/kabekus/photosharingapp/UploadActivity.kt
+++ app/src/main/java/com/kabekus/photosharingapp/UploadActivity.kt
@@ -14,29 +14,52 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.kabekus.photosharingapp.databinding.ActivityUploadBinding
+import java.util.UUID
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding : ActivityUploadBinding
     private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
-
     var selectedPicture : Uri? = null
+    private lateinit var auth : FirebaseAuth
+    private lateinit var firestore : FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         registerLauncher()
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
     }
 
     fun postUpload(view: View){
+        val uuid = UUID.randomUUID()
+        val imageName = "$uuid.jpg"
+        val reference = storage.reference
+        val imageReferences = reference.child("images/$imageName")
+        if(selectedPicture != null){
+            imageReferences.putFile(selectedPicture!!).addOnSuccessListener {
 
+            }.addOnFailureListener {
+                Toast.makeText(this@UploadActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
     }
     fun selectImage(view: View){
         if(ContextCompat.checkSelfPermission(this@UploadActivity,Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
             if(ActivityCompat.shouldShowRequestPermissionRationale(this@UploadActivity,Manifest.permission.READ_MEDIA_IMAGES)){
-                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                Snackbar.make(view,"Galeriye erişim izni gerekli",Snackbar.LENGTH_INDEFINITE).setAction("İzin Ver"){
                         permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
                     }.show()
             }else{
@@ -66,7 +89,7 @@ class UploadActivity : AppCompatActivity() {
                 val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 activityResultLauncher.launch(intentToGallery)
             }else{
-                Toast.makeText(this@UploadActivity,"Uygulamyı Kullanamk İçin İzin Gerkli !",Toast.LENGTH_LONG).show()
+                Toast.makeText(this@UploadActivity,"Uygulamayı Kullanmak İçin İzin Gerkli !",Toast.LENGTH_LONG).show()
 
             }
 
