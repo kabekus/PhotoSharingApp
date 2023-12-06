695dd06c69a4f46d5299642eb706c548c3f76b96
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2023-12-06 17:14:22 +0300

    request permissions created

--- app/src/main/java/com/kabekus/photosharingapp/UploadActivity.kt
+++ app/src/main/java/com/kabekus/photosharingapp/UploadActivity.kt
@@ -1,24 +1,76 @@
 package com.kabekus.photosharingapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
-import com.kabekus.photosharingapp.databinding.ActivityFeedBinding
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.kabekus.photosharingapp.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding : ActivityUploadBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+
+    var selectedPicture : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        registerLauncher()
     }
 
     fun postUpload(view: View){
 
     }
     fun selectImage(view: View){
-
+        if(ContextCompat.checkSelfPermission(this@UploadActivity,Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this@UploadActivity,Manifest.permission.READ_MEDIA_IMAGES)){
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                        permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                    }.show()
+            }else{
+                permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+            }
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+        }
     }
+
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if (result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if (intentFromResult != null){
+                   selectedPicture = intentFromResult.data
+                    selectedPicture?.let {
+                        binding.imageView.setImageURI(it)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result->
+            if (result ){
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                Toast.makeText(this@UploadActivity,"Uygulamyı Kullanamk İçin İzin Gerkli !",Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+    }
+
 }
